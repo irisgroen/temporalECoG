9358640533391006a5d20105d2e545881354e8b3
irisgroen <iris.groen@nyu.edu>
2019-09-13 12:57:07 -0400

    small updates

--- run_tde.m
+++ run_tde.m
@@ -1,17 +1,19 @@
+% script 
 
 [epochs, channels, events] = tde_getData(recomputeflag 0/1);
 
 
-% next script, tde_prepElectrodes
+% next step, tde_prepElectrodes
 % input: epochs, channels, events, taskstouse/trialstouse, inclusion criteria 
 
-% next script, tde_fitModel
+% next step, tde_fitModel
 % input: model: function handle, file?
 % output: model fits
 
-% next script, tde_analyzeModelFits
+% separate script?: tde_analyzePRF, ecog_analyzePRF?
+% preferably NOT another save step, do on the fly? or add to channel table
+
+% next step, tde_analyzeModelFits
 % input: model fits (can be multiple?)
 % outputs: plots, stats
 
-% separate script: tde_analyzePRF
-% preferably NOT another save step, do on the fly? or add to channel table
