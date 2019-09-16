b3b1cfa40e06ed922805b3188a56ec0eb087d3d0
Jonathan Winawer <jonathan.winawer@nyu.edu>
2019-09-16 16:49:00 -0400

    comments

--- run_tde.m
+++ run_tde.m
@@ -6,9 +6,20 @@
 % next step, tde_selectElectrodes
 % input: epochs, channels, events, taskstouse/trialstouse, inclusion criteria 
 
+% next step, tde_generateStimulusTimecourses
+% input: events
+% output: time series per event
+
 % next step, tde_fitModel
-% input: model: function handle, file?
+% input: model: function handle, data, eventCodes, stimulusTimeCourses, opts
 % output: model fits
+
+% -- which models?
+% ----- DN (flavors: uniphasic, biphasic, fixed exponent or not)
+% ----- DN cascade?
+% ----- Two temporal channels (flavors: HH, Stigliani 1, Stigliani 2)
+% ----- DN-like models:
+% --------- Heeger 1993
 
 % separate script?: tde_analyzePRF, ecog_analyzePRF?
 % preferably NOT another save step, do on the fly? or add to channel table
