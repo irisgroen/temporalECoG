8ec4c13662319486d3ed2c630d8f6644dbc16b62
irisgroen <iris.groen@nyu.edu>
2019-12-03 15:30:44 -0500

    simple plotting function

---
+++ tde_plotData.m
@@ -0,0 +1,22 @@
+function tde_plotData(data, channels, t, opts)
+
+% Data should be time x trials x channels
+
+% Plot final selected data
+figure, sz = ceil(sqrt(size(data,3)));
+for ii = 1:size(data,3)
+    subplot(sz,sz,ii); plot(t,data(:,:,ii), 'LineWidth', 2);
+    if opts.average_elecs
+        title(sprintf('%s (n = %d)', ...
+            channels.name{ii}, channels.number_of_elecs{ii})); 
+    else
+        title(sprintf('%s %s %s %s', ...
+            channels.bensonarea{ii}, channels.wangarea{ii}, channels.subject_name{ii}, channels.name{ii})); 
+    end
+    yaxlims = get(gca, 'YLim');
+    line([0 0], [0 yaxlims(2)], 'Color', 'k', 'LineStyle', ':')
+    %line([t(1) t(end)], [0 0],'Color', 'k', 'LineStyle', ':');
+    set(gca, 'YLim', [0 yaxlims(2)]);
+end
+set(gcf, 'Position', [400 200 2000 1200]);
+end
