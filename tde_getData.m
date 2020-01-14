0097eba2aa1a9c88861f3067d9d8e6a0d149622c
irisgroen <iris.groen@nyu.edu>
2020-01-14 10:10:16 -0500

    added sample rate as input parameter

--- tde_getData.m
+++ tde_getData.m
@@ -1,8 +1,8 @@
-function [data] = tde_getData(compute, inputDir, outputDir, subjectList, sessionList, epochTime)
+function [data] = tde_getData(compute, inputDir, outputDir, subjectList, sessionList, epochTime, sampleRate)
 
 % Description: 
 %
-% function [data] = tde_getData(compute, [inputDir], [outputDir], [subjectList], [sessionList], [epochTime])
+% function [data] = tde_getData(compute, [inputDir], [outputDir], [subjectList], [sessionList], [epochTime], [sampleRate])
 %
 % Input (use varargin?)
 % - inputDir
@@ -10,6 +10,7 @@ function [data] = tde_getData(compute, inputDir, outputDir, subjectList, session
 % - subjectslist
 % - sessionList
 % - epochTime
+% - sampleRate
 %
 % Output
 % a cell array with for each cell a struct with the following fields:
@@ -55,6 +56,11 @@ if ~exist('epochTime', 'var') || isempty(epochTime)
     epochTime = [-0.2 1];
 end
 
+% <sampleRate>
+if ~exist('sampleRate', 'var') || isempty(sampleRate)
+    sampleRate = 512;
+end
+
 % <tasks> (fixed for TDE project)
 tasks = {'spatialpattern', 'temporalpattern', 'soc'};
 
@@ -81,7 +87,8 @@ for ii = 1 : length(subjectList)
         fprintf('[%s] Computing data for subject %s \n',mfilename, subject);
 
         %% STEP 0: GET visual area matches for this subject
-        fprintf('[%s] Computing matches with visual atlases...\n',mfilename);
+        fprintf('[%s] Step 0: Computing matches with visual atlases...\n',mfilename);
+        
         specs = [];
         specs.pID           = subject; 
         specs.plotmesh      = 'none';
@@ -89,6 +96,7 @@ for ii = 1 : length(subjectList)
         visualelectrodes    = electrode_to_nearest_node(specs, BIDSformatted);
  
         %% STEP 1: GET THE DATA
+        fprintf('[%s] Step 1: Loading data...\n',mfilename);
 
         [subdata, channels, events] = bidsEcogGetPreprocData(inputDir, subject, sessionList, tasks, [], description);
 
@@ -98,32 +106,35 @@ for ii = 1 : length(subjectList)
         [channels] = bair_addVisualAtlasNamesToChannelTable(channels,visualelectrodes);
 
         % Make selection on visual only, index into data + channels
-        fprintf('[%s] Selecting channels with visual matches...\n',mfilename);
+        fprintf('[%s] Step 2: Selecting channels with visual matches \n',mfilename);
         
         chan_idx1 = find(~contains(channels.wangarea, 'none') & contains(channels.status, 'good'));
         chan_idx2 = find(~contains(channels.bensonarea, 'none') & contains(channels.status, 'good'));        
         chan_idx = unique([chan_idx1; chan_idx2]);
         
-        fprintf('[%s] Found %d channels with visual matches out of %d ecog channels \n', ...
+        fprintf('[%s] Step 2: Found %d channels with visual matches out of %d ecog channels \n', ...
             mfilename, length(chan_idx), length(find(contains(lower(channels.type), {'ecog', 'seeg'}))));
         
         subdata = subdata(chan_idx,:);
         channels = channels(chan_idx,:);
         
-        %% STEP 3: EPOCH THE DATA 
-        fprintf('[%s] Epoching data...\n',mfilename);
+        %% STEP 3: CHECK SAMPLE RATES ANDS SHIFT DATA
         
-        % Resample and SHIFT the UMCU data 
+        fprintf('[%s] Step 3: Checking sample rates...\n',mfilename);
+        
+        if channels.sampling_frequency(1) ~= sampleRate
+            fprintf('[%s] Step 3: Sample rate does not match requested sample rate. Resampling \n',mfilename);
+            subdata = downsample(subdata', channels.sampling_frequency(1)/sampleRate)';
+            events.event_sample = round(events.event_sample/(channels.sampling_frequency(1)/sampleRate));
+            channels.sampling_frequency(:) = sampleRate;
+        end
+        
+        % SHIFT the UMCU data 
         if contains(subject, {'chaam', 'beilen'}) % SHOULD BE READ FROM participants.tsv, if site column = umcu
-            fprintf('[%s] This is a umcu patient: resampling and shifting the onsets\n',mfilename);
-            
-            % Resample data; assuming desired sample rate of 512
-            subdata = downsample(subdata', channels.sampling_frequency(1)/512)';
-            events.event_sample = round(events.event_sample/(channels.sampling_frequency(1)/512));
-            channels.sampling_frequency(:) = 512;
-            
+            fprintf('[%s] Step 3: This is a umcu patient. Shifting onsets \n',mfilename);
+                       
             % Shift onsets
-            shiftInSeconds = 0.072; % 72 ms
+            shiftInSeconds = 0.072; % 72 ms; determined through cross correlation, see s_determineOnsetShiftUMCUvsNYU.m
             shiftInSamples = round(shiftInSeconds*channels.sampling_frequency(1)); 
             events.onset = events.onset + shiftInSeconds;
             events.event_sample = events.event_sample + shiftInSamples; 
@@ -138,27 +149,32 @@ for ii = 1 : length(subjectList)
             end
         end
         
-        % Epoch the data
+        %% STEP 4: EPOCH THE DATA 
+        
+        fprintf('[%s] Step 4: Epoching data \n',mfilename);
+        
         [epochs, t] = ecog_makeEpochs(subdata, events.event_sample, epochTime, channels.sampling_frequency(1));  
         
-        fprintf('[%s] Found %d epochs across %d runs and %d sessions \n', ...
+        fprintf('[%s] Step 4: Found %d epochs across %d runs and %d sessions \n', ...
             mfilename, size(epochs,2), length(unique(events.run_name)), length(unique(events.session_name)));
         
-        %% STEP 4: Save out a single preproc file for each subject 
+        %% STEP 5: Save out a single preproc file for each subject 
         
         % Remove irrelevant/redundant columns from channels and events tables 
-        events = removevars(events,{'onset','event_sample'});
-        channels = removevars(channels,{'notch', 'status'});
+        if isfield(summary(events),'onset'), events = removevars(events,'onset');end
+        if isfield(summary(events),'event_sample'), events = removevars(events,'event_sample');end
         if isfield(summary(events),'stim_file'), events = removevars(events,'stim_file');end
+        if isfield(summary(channels),'notch'), channels = removevars(channels,'notch');end
+        if isfield(summary(channels),'status'), channels = removevars(channels,'status');end
         if isfield(summary(channels),'description'), channels = removevars(channels,'description');end
         if isfield(summary(channels),'status_description'), channels = removevars(channels,'status_description');end
         
-        % Add a subject index column to both:
+        % Add a subject index column to channels and events tables:
         events.subject_name = repmat({subject}, [height(events),1]);
         channels.subject_name = repmat({subject}, [height(channels),1]);
         
         % Save out the data
-        fprintf('[%s] Saving data for subject %s to %s \n',mfilename, subject, outputDir);
+        fprintf('[%s] Step 5: Saving data for subject %s to %s \n',mfilename, subject, outputDir);
         save(fullfile(outputDir, sprintf('%s_data_visualelecs.mat', subject)),'subject', 'epochs', 't', 'events', 'channels')
         
         % Collect into an output struct
