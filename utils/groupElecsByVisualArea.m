c8b29f1edc9da844b7955bf14ace99a4f302aead
irisgroen <iris.groen@nyu.edu>
2020-03-12 11:54:21 -0400

    removed hV4 for now

--- utils/groupElecsByVisualArea.m
+++ utils/groupElecsByVisualArea.m
@@ -7,12 +7,12 @@ INX{2} = contains(channels.wangarea, 'V2') | contains(channels.bensonarea, 'V2')
 INX{3} = contains(channels.wangarea, 'V3') & ~contains(channels.bensonarea, {'V3a', 'V3b'}) | contains(channels.bensonarea, 'V3') & ~contains(channels.bensonarea, {'V3a', 'V3b'});
 INX{4} = contains(channels.wangarea, {'V3a'}) | contains(channels.bensonarea, {'V3a'}) ;
 INX{5} = contains(channels.wangarea, {'V3b'}) | contains(channels.bensonarea, {'V3b'}) ;
-INX{6} = contains(channels.wangarea, {'hV4'}) | contains(channels.bensonarea, {'hV4'});
-INX{7} = contains(channels.wangarea, {'LO1'}) | contains(channels.bensonarea, {'LO1'});
-INX{8} = contains(channels.wangarea, {'LO2'}) | contains(channels.bensonarea, {'LO2'});
-INX{9} = contains(channels.wangarea, {'TO1'}) | contains(channels.bensonarea, {'TO1'});
+%INX{6} = contains(channels.wangarea, {'hV4'}) | contains(channels.bensonarea, {'hV4'});
+INX{6} = contains(channels.wangarea, {'LO1'}) | contains(channels.bensonarea, {'LO1'});
+INX{7} = contains(channels.wangarea, {'LO2'}) | contains(channels.bensonarea, {'LO2'});
+INX{8} = contains(channels.wangarea, {'TO1'}) | contains(channels.bensonarea, {'TO1'});
 %INX{10} = contains(channels.wangarea,{'TO2'}) | contains(channels.bensonarea, {'TO2'});
-INX{10} = contains(channels.wangarea, {'IPS0', 'IPS1', 'IPS2', 'IPS3', 'IPS4', 'IPS5'});
+INX{9} = contains(channels.wangarea, {'IPS0', 'IPS1', 'IPS2', 'IPS3', 'IPS4', 'IPS5'});
 %INX{12} = contains(channels.wangarea, {'VO1','VO2', 'PHC1', 'PHC2'}) | contains(channels.bensonarea, {'VO1', 'VO2'});
 
 subjects = cell(length(INX),1); 
@@ -25,7 +25,8 @@ for ii = 1:length(INX)
 end
 
 % Create a new channels table:
-name               = {'V1', 'V2', 'V3', 'V3a', 'V3b', 'hV4', 'LO1','LO2','TO1','IPS'}';
+%name               = {'V1', 'V2', 'V3', 'V3a', 'V3b', 'hV4', 'LO1','LO2','TO1','IPS'}';
+name               = {'V1', 'V2', 'V3', 'V3a', 'V3b', 'LO1','LO2','TO1','IPS'}';
 type               = repmat({'n/a'}, [length(name) 1]);
 units              = repmat(channels.units(1), [length(name) 1]);
 sampling_frequency = repmat(channels.sampling_frequency(1), [length(name) 1]);
