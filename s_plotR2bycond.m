2926319aa28811077e5b20be37a498216ca8f54c
irisgroen <iris.groen@nyu.edu>
2020-06-04 15:57:45 -0400

    script to plot R2 by condition

--- s_plotR2bycond.m
+++ s_plotR2bycond.m
@@ -27,3 +27,36 @@ for ii = 1:3
     if ii == 3, legend(l); end
 end
 set(gcf, 'Position', [400 200 2000 1200]);
+
+
+%% just V1, all three conditions in one plot
+
+conds       = {'ONEPULSE', 'TWOPULSE','CRF'};
+nChans      = height(channels);
+
+figure;hold on
+l = cellfun(@func2str,{results(:).model}, 'UniformOutput', false);
+
+m = [];
+for jj = 1:length(results)
+    m(:,jj) = results(jj).R2.concat_cond([2 3 1],1);      
+end
+%h = barh(m);
+%h = bar(m);
+h = bar(m,'FaceColor','flat');
+colors = flipud(jet(size(m,2)));
+colors(1,:) = [1 0 0];
+colors(5,:) = [0 0 1];
+for k = 1:size(m,2)
+    h(k).CData = colors(k,:);
+end
+%colormap(jet);
+set(h, 'BarWidth', 1); 
+
+numgroups = size(m,1);
+numbars = size(m,2);
+groupwidth = min(0.8,numbars/(numbars+1.5));
+
+set(gca, 'YLim', [0 1])
+set(gca, 'XTick', 1:3, 'XTickLabel', {'Duration', 'Interval', 'Contrast'});
+%legend(l);
