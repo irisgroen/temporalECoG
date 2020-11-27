94a73a4c71d256a1f91953de8c950f79862bd230
Iris Groen <iris.groen@nyu.edu>
2020-11-27 12:37:08 +0100

    updates

--- utils/averageWithinArea.m
+++ utils/averageWithinArea.m
@@ -1,4 +1,4 @@
-function [m_boot, se] = averageWithinArea(data, group_prob, fun, numboot)
+function [m, se] = averageWithinArea(data, group_prob, fun, numboot)
 % Averages data across electrodes belonging to individual areas. The
 % assignment to areas is based on probability of belonging to each area.
 %
@@ -16,7 +16,7 @@ function [m_boot, se] = averageWithinArea(data, group_prob, fun, numboot)
 %     numboot:          (optional) Number of bootstraps (default: 10000).
 %
 % Output
-%     m:                estimated distribution metric (default median).
+%     m:                estimated summary metric (default median).
 %     se:               68% confidence interval.
 %
 % IG 2020
@@ -43,7 +43,7 @@ end
 [~, nElecs] = size(data);
 m_boot = nan(size(data,1), nAreas, numboot); 
 
-% each boot, sample 112 at once
+% each boot, sample all channels at once
 for ii = 1:numboot
     elec_idx = randsample(1:nElecs,nElecs,1);
     sampled_data = data(:,elec_idx);
@@ -51,16 +51,17 @@ for ii = 1:numboot
     elec_area = assign_to_area(group_prob(elec_idx,:)); 
     % do summary per area - store result
     for jj = 1:nAreas
-        md(:,jj,ii) = fun(sampled_data(:,elec_area == jj),2);
+        m_boot(:,jj,ii) = fun(sampled_data(:,elec_area == jj),2);
     end
 end
 
-m_boot = fun(md,3, 'omitnan');
-se = squeeze(prctile(md,[15.87 84.13],3));
+% take median of bootstrapped distribution and compute confidence intervals
+m = median(m_boot,3, 'omitnan');
+se = squeeze(prctile(m_boot,[15.87 84.13],3));
 
 % reshape back to original shape
 if multiDimData
-    m_boot = reshape(m_boot, [dataSz(1) dataSz(2) nAreas]); 
+    m = reshape(m, [dataSz(1) dataSz(2) nAreas]); 
     se = reshape(se, [dataSz(1) dataSz(2) 2 nAreas]);
 end
 
@@ -68,7 +69,7 @@ end
 
 function elec_area = assign_to_area(group_prob)
     % sample a random number between 0 and 1 
-    p = rand(size(group_prob,1),1);% * ones(1,size(gp,2));
+    p = rand(size(group_prob,1),1); % * ones(1,size(gp,2));
     % compute cumulative sum
     a = cumsum(group_prob,2) > p;
     sa = sum(a,2) < 1;
