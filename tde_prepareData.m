b1939f6c9e7fd78d22241bf23512bd8ae7b3ae53
Iris Groen <iris.groen@nyu.edu>
2020-11-27 12:38:50 +0100

    moved normalization to after electrode averaging step

--- tde_prepareData.m
+++ tde_prepareData.m
@@ -28,17 +28,17 @@ if opts.sort_channels
     epochs = epochs(:,:,I);
 end
 
-% Scale each electrode to its max?
-if opts.normalize_data       
-    [epochs] = normalize_data(epochs);
-end
-
 % Average elecs within area?
 if opts.average_elecs
     fprintf('[%s] Averaging electrodes...\n', mfilename);
     [epochs, channels, epochs_se] = average_elecs(epochs, channels, area_names);
 else
     epochs_se = [];
+end
+
+% Scale each electrode to its max?
+if opts.normalize_data       
+    [epochs] = normalize_data(epochs);
 end
 
 % Add an index column to channels
