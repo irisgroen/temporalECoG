b0f02f42762e7294e6d80288fc21d4e4d74484bb
Iris Groen <iris.groen@nyu.edu>
2021-03-31 11:06:47 +0200

    added dataStr argument

--- mkFigures/tde_loadDataForFigure.m
+++ mkFigures/tde_loadDataForFigure.m
@@ -1,10 +1,19 @@
-function [data] = tde_loadDataForFigure(modelfun, xvalmode, dataType, dataPath)
+function [data] = tde_loadDataForFigure(modelfun, xvalmode, dataType, dataPath, dataStr)
+
+if ~exist('dataStr', 'var')
+    dataStr = [];
+end
 
 if ~exist('dataPath', 'var') || isempty(dataPath)
     dataPath = fullfile(analysisRootPath, 'results');
 end
 
-dataName = sprintf('%s_xvalmode%d_%s', func2str(modelfun), xvalmode, dataType);
-data = load(fullfile(dataPath, dataName));     
+if isempty(dataStr)
+    dataName = sprintf('%s_xvalmode%d_%s', func2str(modelfun), xvalmode, dataType);
+else
+    dataName = sprintf('%s_xvalmode%d_%s_%s', func2str(modelfun), xvalmode, dataType, dataStr);
+end
+
+data = load(fullfile(dataPath, dataName));
    
 end
