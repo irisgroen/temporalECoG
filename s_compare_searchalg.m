66a12974831e8828589cc8b033745faf6a4c01ab
Iris Groen <iris.groen@nyu.edu>
2021-04-14 21:20:52 +0200

    added surrogateopts

--- s_compare_searchalg.m
+++ s_compare_searchalg.m
@@ -1,6 +1,6 @@
 
 % Load data and fits
-modelfun = @DN;%@LINEAR_RECTF_EXP_NORM_DELAY;
+modelfun = @DN; %@LINEAR_RECTF_EXP_NORM_DELAY;%@DN;%
 xvalmode = 0;
 datatype = 'electrodeaverages';
 
@@ -8,7 +8,11 @@ datastr = 'bads';
 [D] = tde_loadDataForFigure(modelfun, xvalmode, datatype, [], datastr);
 [results_bads] = tde_evaluateModelFit(D,0);
 
-datastr = 'lsq';
+datastr = 'surrogateopts';
+[D] = tde_loadDataForFigure(modelfun, xvalmode, datatype, [], datastr);
+[results_surr] = tde_evaluateModelFit(D,0);
+
+datastr = 'lsqnonlin';
 [D] = tde_loadDataForFigure(modelfun, xvalmode, datatype, [], datastr);
 [results_lsq] = tde_evaluateModelFit(D,0);
 
@@ -21,19 +25,23 @@ T = strsplit(p.params, ',');
 
 figure, hold on,
 subplot(2,5,1); hold on
-plot(results_bads.R2.concat_all, 'LineWidth', 2);
-plot(results_lsq.R2.concat_all, 'LineWidth', 2);
-plot(results_fmincon.R2.concat_all, 'LineWidth', 2);
+plot(results_bads.R2.concat_all,'-o', 'LineWidth', 2);
+plot(results_surr.R2.concat_all, '-.s','LineWidth', 2);
+plot(results_lsq.R2.concat_all, '--d', 'LineWidth', 2);
+plot(results_fmincon.R2.concat_all, ':x', 'LineWidth', 2);
 ylim([0 1]);
 
-legend('bads', 'lsqnonlin', 'fmincon'); xlabel('electrode'); ylabel('fitted value');
+legend('bads','surrogateopts', 'lsqnonlin', 'fmincon'); xlabel('electrode'); ylabel('fitted value');
+%legend('bads','lsqnonlin', 'fmincon'); xlabel('electrode'); ylabel('fitted value');
+
 title('R2');
 
 for ii = 1:length(T)
     subplot(2,5,ii+1); hold on;
-    plot(results_bads.params(ii,:), 'LineWidth', 2);
-    plot(results_lsq.params(ii,:), 'LineWidth', 2);
-    plot(results_fmincon.params(ii,:), 'LineWidth', 2);
+    plot(results_bads.params(ii,:),'-o', 'LineWidth', 2);
+	plot(results_surr.params(ii,:), '--d', 'LineWidth', 2);
+    plot(results_lsq.params(ii,:), '-.s', 'LineWidth', 2);
+    plot(results_fmincon.params(ii,:),  ':x','LineWidth', 2);
     title(T{ii});
 end
 
