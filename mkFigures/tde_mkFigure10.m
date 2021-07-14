0e4fc00ba3fdf798be4c8d75b714bb7398296ddf
Iris Groen <iris.groen@nyu.edu>
2021-07-14 15:11:27 +0200

    updates to contrast comparison higher areas

--- mkFigures/tde_mkFigure10.m
+++ mkFigures/tde_mkFigure10.m
@@ -16,10 +16,11 @@ if D.options.fitaverage
     [data, data_se, channels] = averageMultipleFits(D.data, D.channels, @mean);
     [pred, pred_se] = averageMultipleFits(D.pred, D.channels, @mean);
 else
-    % this means we have just one fit to all individual electrodes 
+    % this means we have just one fit to all individual electrodes
+    numboots = 10000;
     [~, channels, group_prob] = groupElecsByVisualArea(D.channels, 'probabilisticresample');   
-    [data, data_se] = averageWithinArea(D.data, group_prob, @mean, 1000);
-    [pred, pred_se] = averageWithinArea(D.pred, group_prob, @mean, 1000);
+    [data, data_se] = averageWithinArea(D.data, group_prob, @mean, numboots);
+    [pred, pred_se] = averageWithinArea(D.pred, group_prob, @mean, numboots);
 end
 t = D.t;
 stim_ts = D.stim;
@@ -106,41 +107,43 @@ set(findall(gcf,'-property','FontSize'),'FontSize',20)
 
 % Data
 
-% Determine time index over which to compute sum
+% Determine time index over which to compute max
 t_idx = t>0.05 & t<1.0;
 
-% Concatenate data and prediction (in order to make sure probabilistic
-% assignment of electrodes to areas is done the same way for both).
+% Get data
 d = D.data(t_idx,stim_idx,:);
+p = D.pred(t_idx,stim_idx,:);
 
-%d_s = smooth(d(:),10);
-%d = reshape(d_s, size(d));
+% Compute max across stim_on window
+maxd = squeeze(max(d,[],1));
+maxp = squeeze(max(p,[],1));
 
-% Compute sum across stim_on window
-sumd = squeeze(max(d,[],1));
-%sumd = squeeze(sum(d,1));
-
-c50_data = nan(1,height(D.channels));
+c50_data  = nan(1,height(D.channels));
+c50_model = nan(1,height(D.channels));
 fprintf('[%s] Fitting Naka Rushton function to each dataset...\n',mfilename);
 for ii = 1:height(D.channels)
-    [~,c50_data(ii)] = fitNakaRushton(stim_info.contrast(stim_idx)*100,sumd(:,ii),0);
+    [~,c50_data(ii)] = fitNakaRushton(stim_info.contrast(stim_idx)*100,maxd(:,ii),0);
+    [~,c50_model(ii)] = fitNakaRushton(stim_info.contrast(stim_idx)*100,maxp(:,ii),0);
 end
 
 if D.options.fitaverage
     [m_data, se_data, channels] = averageMultipleFits(c50_data, D.channels, @mean);
+	[m_model, se_model] = averageMultipleFits(c50_model, D.channels, @mean);
 else
-    [m_data, se_data] = averageWithinArea(c50_data, group_prob, @mean, 1000);
+    [m_data, se_data] = averageWithinArea(c50_data, group_prob, @median, numboots);
+    [m_model, se_model] = averageWithinArea(c50_model, group_prob, @median, numboots);
 end
 
-% Model
-c50_model = results.derived.params(4,:);
+% % Model
+% c50_model = results.derived.params(4,:);
+% 
+% if D.options.fitaverage
+%     [m_model, se_model, channels] = averageMultipleFits(c50_model, D.channels, @mean);
+% else
+%     [m_model, se_model] = averageWithinArea(c50_model, group_prob, @median, numboots);
+% end
 
-if D.options.fitaverage
-    [m_model, se_model, channels] = averageMultipleFits(c50_model, D.channels, @mean);
-else
-    [m_model, se_model] = averageWithinArea(c50_model, group_prob, @mean, 1000);
-end
-
+% Plot
 subplot('position', posb); cla; hold on
 x = 1:height(channels);
 tde_plotPoints(m_model', se_model, x, 'ci', 0, [], 40, 'r');
@@ -150,97 +153,277 @@ set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
 ylabel('C50 (% contrast)');
 xlabel('Visual area');
 
-xlim([0 max(x)+1]); ylim([0 100]);
-legend('model', 'data', 'Location','NorthWest');legend boxoff
+xlim([0 max(x)+1]); ylim([0 50]);
+legend('model', 'data', 'location','northeast');legend boxoff
    
+%% Panel C
+% figure;hold on
+% set(gcf, 'position',  get(0, 'screensize'));
+% 
+% % % Data
+% 
+% % Get data
+% d = D.data(:,stim_idx,:);
+% p = D.pred(:,stim_idx,:);
+% 
+% % Smooth time courses to get better estimates of max and offset response levels
+% for ii = 1:size(d,2)
+%     for jj = 1:size(d,3)
+%         d(:,ii,jj) = smooth(d(:,ii,jj),100);
+%         p(:,ii,jj) = smooth(p(:,ii,jj),100);
+%     end
+% end
+% 
+% [Md] = max(d,[],1); % value at peak
+% [Mp] = max(p,[],1);
+% t_off = (t == 0.5); % offset timepoint
+% Od = d(t_off,:,:); % value at offset
+% Op = p(t_off,:,:); % value at offset
+% Rd = squeeze(Od./Md); % divide value at offset with value at peak
+% Rp = squeeze(Op./Mp); % divide value at offset with value at peak
+% cov_Rd = std(Rd)./mean(Rd);
+% cov_Rp = std(Rp)./mean(Rp);
+% 
+% if D.options.fitaverage
+%     [R_data, Rse_data, channels] = averageMultipleFits(Rd, D.channels, @mean);
+%     [R_pred, Rse_pred] = averageMultipleFits(Rp, D.channels, @mean);
+%     [Rcov_data, Rcovse_data] = averageMultipleFits(cov_Rd, D.channels, @mean);
+%     [Rcov_pred, Rcovse_pred] = averageMultipleFits(cov_Rp, D.channels, @mean);
+% else
+%     [R_data, Rse_data] = averageWithinArea(Rd, group_prob, @median, numboots);
+%     [R_pred, Rse_pred] = averageWithinArea(Rp, group_prob, @median, numboots);
+% end
+% 
+% cmap      = brewermap(height(channels)+2, '*YlOrRd');
+% cmap      = cmap(1:height(channels),:);
+% 
+% % Plot
+% %subplot('position', posc); cla; hold on
+% subplot(1,3,1); hold on;
+% chan_ind = 1:height(channels);
+% 
+% for ii = chan_ind
+%     %tde_plotPoints(R_data(:,ii), squeeze(Rse_data(:,ii,:)),stim_info.contrast(stim_idx)*100, 'ci', 0, [],[],cmap(ii,:));
+%     tde_plotPoints(R_data(:,ii), [],stim_info.contrast(stim_idx)*100, 'ci', 0, [],[],cmap(ii,:));
+% end
+% 
+% % Format axes
+% ylim([0 1.1]);
+% xlim([0 105]);
+% %set(gca, 'xtick', stim_info.contrast(stim_idx)*100); % in ms
+% xlabel('Contrast');
+% ylabel('Offset/peak');
+% legend(channels.name(chan_ind), 'location', 'northeast');
+% legend boxoff
+% % % Model
+% 
+% subplot(1,3,2); hold on;
+% for ii = chan_ind
+%     %tde_plotPoints(R_pred(:,ii), squeeze(Rse_pred(:,ii,:)),stim_info.contrast(stim_idx)*100, 'ci', 0, [],[],cmap(ii,:));
+%     tde_plotPoints(R_pred(:,ii), [],stim_info.contrast(stim_idx)*100, 'ci', 0, [],[],cmap(ii,:));
+% end
+% 
+% % Format axes
+% ylim([0 1.1]);
+% xlim([0 105]);
+% %set(gca, 'xtick', stim_info.contrast(stim_idx)*100); % in ms
+% xlabel('Contrast');
+% ylabel('Offset/peak');
+% 
+% % Compute variance across ratios per area
+% subplot(1,3,3); hold on;
+% 
+% x = 1:height(channels);
+% tde_plotPoints(Rcov_pred', Rcovse_pred, x, 'ci', 0, [], 40, 'r');
+% tde_plotPoints(Rcov_data', Rcovse_data, x, 'errbar', 0, [], 40, 'k');
+% 
+% set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
+% ylabel('COV in offset/peak over conditions');
+% xlabel('Visual area');
+% 
+% xlim([0 max(x)+1]); ylim([0 1]);
+% legend('model', 'data', 'location','northeast');legend boxoff
+% 
+% set(findall(gcf,'-property','FontSize'),'FontSize',24)
+
+%% Panel D
+
+% Determine time index over which to compute correlation
+t_idx = t>-0.05 & t<1.0;
+
+% Get data
+d = D.data(t_idx,stim_idx,:);
+p = D.pred(t_idx,stim_idx,:);
+
+% % Smooth time courses to get better estimates of max and offset response levels
+% for ii = 1:size(d,2)
+%     for jj = 1:size(d,3)
+%         d(:,ii,jj) = smooth(d(:,ii,jj),100);
+%         p(:,ii,jj) = smooth(p(:,ii,jj),100);
+%     end
+% end
+
+% % DEBUG
+for ii = 1:9%size(d,3)
+    test = d(:,:,ii)./max(d(:,3,ii),[],1);
+    figure; hold on
+    subplot(1,2,1);hold on
+    plot(test);
+    title(D.channels.name(ii));
+    subplot(1,2,2);hold on;
+    tmp = pdist(test','euclidean'); 
+    imagesc(squareform(tmp));colorbar;caxis([0 5]);
+    axis tight
+end
+
+% Correlation distance
+meanDISTdata = [];
+meanDISTpred = [];
+for ii = 1:size(d,3)
+    tmpd = d(:,:,ii)./max(d(:,3,ii),[],1);
+    tmpp = p(:,:,ii)./max(p(:,3,ii),[],1);
+    DISTdata = pdist(tmpd','correlation'); 
+    DISTpred = pdist(tmpp','correlation'); 
+    meanDISTdata(ii) = mean(DISTdata);
+    meanDISTpred(ii) = mean(DISTpred);   
+end
+    
+[dist_data, distse_data, channels] = averageMultipleFits(meanDISTdata, D.channels, @mean);
+[dist_pred, distse_pred] = averageMultipleFits(meanDISTpred, D.channels, @mean);
+
+figure;hold on
+x = 1:height(channels);
+tde_plotPoints(dist_data', distse_data, x, 'ci', 0, [], 40, 'r');
+tde_plotPoints(dist_pred', distse_pred, x, 'errbar', 0, [], 40, 'k');
+
+set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
+ylabel('average distance');
+xlabel('Visual area');
+title('correlation distance');
+
+xlim([0 max(x)+1]); %ylim([0 1]);
+legend('model', 'data', 'location','northeast');legend boxoff
+
+% Euclidean distance
+meanDISTdata = [];
+meanDISTpred = [];
+for ii = 1:size(d,3)
+    tmpd = d(:,:,ii)./max(d(:,1,ii),[],1);
+    tmpp = p(:,:,ii)./max(p(:,1,ii),[],1);
+    DISTdata = pdist(tmpd','euclidean'); 
+    DISTpred = pdist(tmpp','euclidean'); 
+    meanDISTdata(ii) = mean(DISTdata);
+    meanDISTpred(ii) = mean(DISTpred);   
+end
+    
+[dist_data, distse_data, channels] = averageMultipleFits(meanDISTdata, D.channels, @mean);
+[dist_pred, distse_pred] = averageMultipleFits(meanDISTpred, D.channels, @mean);
+
+figure;hold on
+x = 1:height(channels);
+tde_plotPoints(dist_data', distse_data, x, 'ci', 0, [], 40, 'r');
+tde_plotPoints(dist_pred', distse_pred, x, 'errbar', 0, [], 40, 'k');
+
+set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
+ylabel('average distance');
+xlabel('Visual area');
+
+xlim([0 max(x)+1]); %ylim([0 1]);
+legend('model', 'data', 'location','northeast');legend boxoff
+title('euclidean distance');
+% Compute correlation distance between all 5 contrast conditions
+% Make matrix
+% Average off-diagonal distances
+% Plot average
+
+%set(findall(gcf,'-property','FontSize'),'FontSize',24)
+
 %% Panel C: scaling error across areas
-
-% Data
-p = D.pred(:,stim_idx,:);
-cScale_data = nan(2,height(D.channels));
-cScale_model = nan(2,height(D.channels));
-fprintf('[%s] Regressing low and high contrast conditions for each dataset...\n',mfilename);
-for ii = 1:height(D.channels)
-    %X = d(:,1,ii)/max(d(:,1,ii)); % 6.25% contrast
-    %y = d(:,5,ii)/max(d(:,5,ii)); % 100% contrast
-    X = d(:,1,ii); % 6.25% contrast
-    y = d(:,5,ii); % 100% contrast
-    
-    %mdl = fitlm(X,y,'Intercept',false);
-    mdl = fitlm(X,y);
-    cScale_data(1,ii) = sum(abs(mdl.Residuals.Raw)); 
-    cScale_data(2,ii) = mdl.Rsquared.Ordinary;
-
-   % tmp = pdist([X y]', 'correlation');
-   % cScale_data(1,ii) = tmp; 
-   
-    %X = p(:,1,ii)/max(p(:,1,ii)); % 6.25% contrast
-    %y = p(:,5,ii)/max(p(:,5,ii)); % 100% contrast
-    X = p(:,1,ii); % 6.25% contrast
-    y = p(:,5,ii); % 100% contrast
-    
-   % tmp = pdist([X y]', 'correlation');
-   % cScale_model(1,ii) = tmp; 
-	%mdl = fitlm(X,y,'Intercept',false);
-	mdl = fitlm(X,y);
-
-    cScale_model(1,ii) = sum(abs(mdl.Residuals.Raw)); 
-    cScale_model(2,ii) = mdl.Rsquared.Ordinary;
-
-end
-%figure;plot(t(t_idx),X,t(t_idx),y);
-%title(tmp);
-%tmp = mdl.Residuals.Raw;
-%figure;plot(t(t_idx),X,t(t_idx),y,t(t_idx),tmp)
-%legend('low','high','resid')
-%title(mdl.Rsquared.Ordinary);
-
-if D.options.fitaverage
-    [m_data, se_data, channels] = averageMultipleFits(cScale_data, D.channels, @mean);
-else
-    [m_data, se_data] = averageWithinArea(cScale_data, group_prob, @mean, 1000);
-end
-
-% Model
-%cScale_model = results.derived.params([5 6],:);
-
-if D.options.fitaverage
-    [m_model, se_model, channels] = averageMultipleFits(cScale_model, D.channels, @mean);
-else
-    [m_model, se_model] = averageWithinArea(cScale_model, group_prob, @mean, 1000);
-end
-
-%% Plot
-subplot('position', posc); cla; hold on
-x = 1:height(channels);
-tde_plotPoints(m_model(1,:)'./m_model(1,1), squeeze(se_model(1,:,:))./m_model(1,1), x, 'ci', 0, [], 40, 'r');
-tde_plotPoints(m_data(1,:)'./m_data(1,1), squeeze(se_data(1,:,:))./m_data(1,1), x, 'errbar', 0, [], 40, 'k');
-%tde_plotPoints(m_model(1,:)', squeeze(se_model(1,:,:)), x, 'ci', 0, [], 40, 'r');
-%tde_plotPoints(m_data(1,:)', squeeze(se_data(1,:,:)), x, 'errbar', 0, [], 40, 'k');
-
-xlim([0 max(x)+1]); ylim([0 1.5]);
-
-set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
-ylabel('residuals regression (summed)');
-xlabel('Visual area');
-legend('model', 'data', 'Location','NorthEast');legend boxoff
-
-%%
-subplot('position', posd); cla; hold on
-x = 1:height(channels);
-shift = m_model(2,1)-m_data(2,1);
-%tde_plotPoints(m_model(2,:)'./max(m_model(2,:)), squeeze(se_model(2,:,:))./max(m_data(2,:)), x, 'ci', 0, [], 40, 'r');
-%tde_plotPoints(m_data(2,:)'./max(m_data(2,:)), squeeze(se_data(2,:,:))./max(m_data(2,:)), x, 'errbar', 0, [], 40, 'k');
-
-tde_plotPoints(m_model(2,:)'-shift, squeeze(se_model(2,:,:))-shift, x, 'ci', 0, [], 40, 'r');
-tde_plotPoints(m_data(2,:)', squeeze(se_data(2,:,:)), x, 'errbar', 0, [], 40, 'k');
-xlim([0 max(x)+1]); ylim([-0.1 1]);
-
-set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
-ylabel('R2 regression');
-xlabel('Visual area');
-
-
-set(findall(gcf,'-property','FontSize'),'FontSize',24)
-
+% % Data
+% p = D.pred(:,stim_idx,:);
+% cScale_data = nan(2,height(D.channels));
+% cScale_model = nan(2,height(D.channels));
+% fprintf('[%s] Regressing low and high contrast conditions for each dataset...\n',mfilename);
+% for ii = 1:height(D.channels)
+%     %X = d(:,1,ii)/max(d(:,1,ii)); % 6.25% contrast
+%     %y = d(:,5,ii)/max(d(:,5,ii)); % 100% contrast
+%     X = d(:,1,ii); % 6.25% contrast
+%     y = d(:,5,ii); % 100% contrast
+%     
+%     mdl = fitlm(X,y,'Intercept',false);
+%     %mdl = fitlm(X,y);
+%     cScale_data(1,ii) = sum(abs(mdl.Residuals.Standardized)); 
+%     cScale_data(2,ii) = mdl.Rsquared.Ordinary;
+% 
+%    % tmp = pdist([X y]', 'correlation');
+%    % cScale_data(1,ii) = tmp; 
+%    
+%     %X = p(:,1,ii)/max(p(:,1,ii)); % 6.25% contrast
+%     %y = p(:,5,ii)/max(p(:,5,ii)); % 100% contrast
+%     X = p(:,1,ii); % 6.25% contrast
+%     y = p(:,5,ii); % 100% contrast
+%     
+%    % tmp = pdist([X y]', 'correlation');
+%    % cScale_model(1,ii) = tmp; 
+% 	mdl = fitlm(X,y,'Intercept',false);
+% 	%mdl = fitlm(X,y);
+% 
+%     cScale_model(1,ii) = sum(abs(mdl.Residuals.Standardized)); 
+%     cScale_model(2,ii) = mdl.Rsquared.Ordinary;
+% 
+% end
+% %figure;plot(t(t_idx),X,t(t_idx),y);
+% %title(tmp);
+% %tmp = mdl.Residuals.Raw;
+% %figure;plot(t(t_idx),X,t(t_idx),y,t(t_idx),tmp)
+% %legend('low','high','resid')
+% %title(mdl.Rsquared.Ordinary);
+% 
+% if D.options.fitaverage
+%     [m_data, se_data, channels] = averageMultipleFits(cScale_data, D.channels, @mean);
+% else
+%     [m_data, se_data] = averageWithinArea(cScale_data, group_prob, @mean, numboots);
+% end
+% 
+% % Model
+% %cScale_model = results.derived.params([5 6],:);
+% 
+% if D.options.fitaverage
+%     [m_model, se_model, channels] = averageMultipleFits(cScale_model, D.channels, @mean);
+% else
+%     [m_model, se_model] = averageWithinArea(cScale_model, group_prob, @mean, numboots);
+% end
+% 
+% %% Plot
+% subplot('position', posc); cla; hold on
+% x = 1:height(channels);
+% tde_plotPoints(m_model(1,:)'./m_model(1,1), squeeze(se_model(1,:,:))./m_model(1,1), x, 'ci', 0, [], 40, 'r');
+% tde_plotPoints(m_data(1,:)'./m_data(1,1), squeeze(se_data(1,:,:))./m_data(1,1), x, 'errbar', 0, [], 40, 'k');
+% %tde_plotPoints(m_model(1,:)', squeeze(se_model(1,:,:)), x, 'ci', 0, [], 40, 'r');
+% %tde_plotPoints(m_data(1,:)', squeeze(se_data(1,:,:)), x, 'errbar', 0, [], 40, 'k');
+% 
+% xlim([0 max(x)+1]); ylim([0 3]);
+% 
+% set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
+% ylabel('residuals regression (summed)');
+% xlabel('Visual area');
+% legend('model', 'data', 'Location','NorthEast');legend boxoff
+% 
+% %%
+% subplot('position', posd); cla; hold on
+% x = 1:height(channels);
+% shift = m_model(2,1)-m_data(2,1);
+% %tde_plotPoints(m_model(2,:)'./max(m_model(2,:)), squeeze(se_model(2,:,:))./max(m_data(2,:)), x, 'ci', 0, [], 40, 'r');
+% %tde_plotPoints(m_data(2,:)'./max(m_data(2,:)), squeeze(se_data(2,:,:))./max(m_data(2,:)), x, 'errbar', 0, [], 40, 'k');
+% 
+% tde_plotPoints(m_model(2,:)'-shift, squeeze(se_model(2,:,:))-shift, x, 'ci', 0, [], 40, 'r');
+% tde_plotPoints(m_data(2,:)', squeeze(se_data(2,:,:)), x, 'errbar', 0, [], 40, 'k');
+% xlim([0 max(x)+1]); ylim([-0.1 1]);
+% 
+% set(gca, 'xtick', x, 'xticklabel', channels.name, 'xticklabelrotation', 45);
+% ylabel('R2 regression');
+% xlabel('Visual area');
+% 
+% 
+% 
 
--- utils/fitNakaRushton.m
+++ utils/fitNakaRushton.m
@@ -9,37 +9,35 @@ rMid = ((max(r)-min(r))/2) + min(r);
 [~,rMidIndex] = min(abs(r-rMid));
 initC50 = c(rMidIndex(1));
 
+% Set starting points and bounds:
 
-% set starting points and bounds:
 % c50 n offset rmax
-sp = [initC50 2 min(r) rMid];
-lb = [0 1 -inf -inf];
-ub = [max(c) 5 min(r) max(r)];
+sp = [initC50 2 0 rMid];
+lb = [0 1 0 -inf];
+ub = [max(c) 5 0 max(r)];
+% Note that we fix the offset to be zero, forcing the function to go
+% through 0.
 
-% formula_to_fit = 'rmax * ((x.^n)./((x.^n)+c50.^n))+o';
-% f = fit(c, r, formula_to_fit,  'StartPoint', sp, 'Lower', lb, 'Upper', ub);
-% c50 = f.c50;
-% rmax = f.rmax;
-% n = f.n;
-% offset = f.o;
-
+% Define function and algorithm
 searchopts = optimoptions(@lsqcurvefit,'Algorithm','levenberg-marquardt', 'Display', 'off');
 fun = @(x,c)x(4)* ((c.^x(2))./((c.^x(2))+x(1).^x(2)))+x(3);
+
+% Fit function
 x = lsqcurvefit(fun,sp,c,r,lb,ub,searchopts);
  
+% Save out parameters
 c50 = x(1);
 n = x(2);
 offset = x(3);
 rmax = x(4);
 f = fun;
 
+% Plot
 if makePlot
-    %x1 = 0:max(c)/1000:max(c)*2;
-    %y1 = f(x1);
     c1 = 0:max(c)/1000:max(c)*2;
     y1 = fun(x,c1);
     figure;hold on
-    plot(c,r, x1, y1);%pause(1);%close;
+    plot(c,r, c1, y1);%pause(1);%close;
     line([c50 c50], [0 rmax],'LineStyle', ':', 'Color', 'k');
 end
 
@@ -53,3 +51,12 @@ end
 % initParams = [max(r)        initC50 2  min(r)];
 % minParams =  [0             0       1  -inf];
 % maxParams =  [inf           1       5  inf];
+
+% % OLD (using different fitting function, yielding similar results)
+% formula_to_fit = 'rmax * ((x.^n)./((x.^n)+c50.^n))+o';
+% f = fit(c, r, formula_to_fit,  'StartPoint', sp, 'Lower', lb, 'Upper', ub);
+% c50 = f.c50;
+% rmax = f.rmax;
+% n = f.n;
+% offset = f.o;
+
